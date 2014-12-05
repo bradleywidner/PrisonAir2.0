7025af8f1ab81881bfe841974f94baa1c87bd37e
Bradley Widner <bradley.widner@hotmail.com>
2014-12-05 14:42:19 -0500

    Added extras
    Explosions and tags for all objects,  No longer billboards will stop
    gameplay - Moved boat, and downloaded assets from store

---
+++ Assets/Scripts/ExplosionBarrel.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplosionBarrel : MonoBehaviour {
+
+	public Transform explosionPosition;
+	public GameObject explosionEffect;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnCollisionEnter(Collision theplayer)
+	{
+		Debug.Log(theplayer.gameObject.name);
+		if(theplayer.gameObject.name.Contains("MiddleSpine"))
+		{
+			Instantiate(explosionEffect,explosionPosition.position,explosionPosition.rotation);
+			Debug.Log ("Entered");
+			//theplayer.rigidbody.AddExplosionForce(4000f,new Vector3(explosionPosition.position.x,
+		                                                           //explosionPosition.position.y,
+		                                                           //explosionPosition.position.z),250f);
+		}
+	}
+}
